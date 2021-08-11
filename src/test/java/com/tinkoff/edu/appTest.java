9f57330aaf4f1b7e2115f8b37156383cd53f9f06
fromonday <ilya.shpachenko@gmail.com>
2021-08-11 23:04:15 +0300

    add maven project

---
+++ src/test/java/com/tinkoff/edu/appTest.java
@@ -0,0 +1,12 @@
+package com.tinkoff.edu;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class appTest{
+    @Test
+    public void shouldAnswerWithTrue(){
+        assertTrue(true);
+    }
+}
