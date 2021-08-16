e728342138a2a2c815d756b8a66535af095d6b38
fromonday <ilya.shpachenko@gmail.com>
2021-08-16 21:38:03 +0300

    add classes: LoanCalcService, LoanCalcRepository, LoanCalcLogger

---
+++ src/main/java/com/tinkoff/edu/app/LoanCalcService.java
@@ -0,0 +1,13 @@
+package com.tinkoff.edu.app;
+
+public class LoanCalcService{
+    /**
+     * Loan calculation
+     * @return
+     */
+    public static int createRequest() {
+        int localVar = 0;
+        LoanCalcRepository.save();
+        return localVar;
+    }
+}
